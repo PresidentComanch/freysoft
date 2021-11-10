123a0300186b16992dc8c5293f88afe123ce1673
PresidentComanch <ap.dissembler@gmail.com>
2021-11-10 15:10:31 +0200

    refactored before sending

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import About from './About';
 import Advantages from './Advantages';
-import Application from './Application';
-import OpenVacancies from './OpenVacancies';
-import ProsOfRecruitmentProcess from './ProsOfRecruitmentProcess';
-import RecruitmentProcess from './RecrutmentProcess';
 
 import '../styles/main.scss';
 
@@ -12,11 +8,7 @@ function Main() {
   return (
     <main className="main">
       <About />
-      <RecruitmentProcess />
-      <ProsOfRecruitmentProcess />
-      <OpenVacancies />
       <Advantages />
-      <Application />
     </main>
   );
 }
